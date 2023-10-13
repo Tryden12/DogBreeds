64bb22642a1c8e909d32c4fb9e362714bf949b8b
Tyler Ryden <tryden11@gmail.com>
2023-10-13 10:42:11 -0400

    Application class created

---
+++ app/src/main/java/com/tryden/dogbreeds/DogBreedsApplication.kt
@@ -0,0 +1,11 @@
+package com.tryden.dogbreeds
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+/**
+ * Author: Tyler Ryden
+ * This is the Application class which runs before any class.
+ */
+@HiltAndroidApp
+class DogBreedsApplication: Application()
